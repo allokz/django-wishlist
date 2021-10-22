960608479f895a636d2e02d05b539a82cc0051fe
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2021-10-22 22:21:16 +0200

    login page created
    added url config and template for login page

--- wudi/urls.py
+++ wudi/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('wishlist.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
