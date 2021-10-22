e35ed644282607f9b2b9a03b4cd948fb2946f768
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2021-10-22 21:53:37 +0200

    Update models.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 import uuid
 
 
@@ -8,13 +9,13 @@ class CustomUser(AbstractUser):
     birthday = models.DateField(null=True, blank=True)
 
 class Wish(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=40)
     description = models.TextField('Beschreibung', max_length=90, blank=True)
     image = models.ImageField(blank=True)
     shop_url = models.URLField('Link', blank=True, help_text='Link zu einem Shop o.ä.')
     price = models.DecimalField('Preis', max_digits=6, decimal_places=2, blank=True)
-    gifter = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    gifter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='gifter', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
