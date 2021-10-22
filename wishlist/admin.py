0d5c9156db888ca5d77227863806d9c5e67de45a
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2021-10-23 00:35:54 +0200

    updated wish model view on django admin panel

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -3,4 +3,7 @@ from wishlist.models import CustomUser, Wish
 
 
 admin.site.register(CustomUser)
-admin.site.register(Wish)
+
+@admin.register(Wish)
+class WishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'user')
