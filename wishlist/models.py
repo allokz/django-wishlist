ba96ca30933c1b96484131244835792b7ae7705c
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2021-10-23 19:40:06 +0200

    serving media images enabled, wish model updated

--- wishlist/models.py
+++ wishlist/models.py
@@ -12,7 +12,7 @@ class Wish(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=40)
     description = models.TextField('Beschreibung', max_length=90, blank=True)
-    image = models.ImageField('Bild', blank=True)
+    image = models.ImageField('Bild', upload_to='images/', blank=True)
     shop_url = models.URLField('Link', blank=True, help_text='Link zu einem Shop o.ä.')
     price = models.DecimalField('Preis', max_digits=6, decimal_places=2, blank=True)
     gifter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='gifter', on_delete=models.SET_NULL, null=True, blank=True)
