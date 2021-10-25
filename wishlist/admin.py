51ec6b8c95938095f2a71e7673de6352bafb729f
Alexander Schenk <84243644+yodur@users.noreply.github.com>
2021-10-25 13:12:39 +0200

    custom user admin panel (with new fields)

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -6,6 +6,20 @@ from wishlist.models import CustomUser, Wish
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'first_name', 'last_name', 'id')
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password', 'id')
+        }),
+        ('Persönliche Informationen', {
+            'fields': ('first_name', 'last_name', 'email', 'birthday', 'image')
+        }),
+        ('Berechtigungen', {
+            'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Wichtige Daten', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
 
 @admin.register(Wish)
 class WishAdmin(admin.ModelAdmin):
